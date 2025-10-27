830af0daf725cc94999886925dae12809269b964
iman-nab <52522998+iman-nab@users.noreply.github.com>
2025-10-27 18:50:56 +0100

    Add files via upload

---
+++ wanguard/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import WanguardPage from './index.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WanguardPage />
+  </React.StrictMode>,
+)
---
+++ wanguard/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Vite config for Wanguard one-pager
+export default defineConfig({
+  plugins: [react()],
+})
